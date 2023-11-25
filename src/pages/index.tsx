85228aac0a6d84ec2259f6386c6d505415b0e41a
Neil Alvarez Garcia <Neil8b.jjr2014@gmail.com>
2023-11-25 18:23:50 -0500

    adding charts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,80 @@ import Head from 'next/head';
 import s from '@/styles/Home.module.css';
 import { DashboardLayout, SectionLayout } from '@/commons/layouts';
 import { salesData } from '@/mocks';
+import { Bar, Pie } from 'react-chartjs-2';
+
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  ArcElement,
+} from 'chart.js';
+
+ChartJS.register(ArcElement, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
+
+const options = {
+  responsive: true,
+  resizeBy: '',
+  plugins: {
+    legend: {
+      position: 'top' as const,
+    },
+    title: {
+      display: true,
+      text: 'Chart.js Bar Chart',
+    },
+  },
+};
+
+const UserData = [
+  {
+    id: 1,
+    year: 2016,
+    userGain: 80000,
+    userLost: 823,
+  },
+  {
+    id: 2,
+    year: 2017,
+    userGain: 45677,
+    userLost: 345,
+  },
+  {
+    id: 3,
+    year: 2018,
+    userGain: 78888,
+    userLost: 555,
+  },
+  {
+    id: 4,
+    year: 2019,
+    userGain: 90000,
+    userLost: 4555,
+  },
+  {
+    id: 5,
+    year: 2020,
+    userGain: 4300,
+    userLost: 234,
+  },
+];
+
+export const data = {
+  labels: UserData.map((data) => data.year),
+  datasets: [
+    {
+      label: 'Users Gained',
+      data: UserData.map((data) => data.userGain),
+      backgroundColor: ['rgba(75,192,192,1)', '#ecf0f1', '#50AF95', '#f3ba2f', '#2a71d0'],
+      borderColor: 'black',
+      borderWidth: 2,
+    },
+  ],
+};
 
 const Home = () => {
   return (
@@ -30,11 +104,19 @@ const Home = () => {
 
           <div className={s.content}>
             <div className={s.leftSideContent}>
-              <SectionLayout title='Ventas de los últimos 7 días'></SectionLayout>
+              <SectionLayout title='Ventas de los últimos 7 días'>
+                <div className={s.chartContainer}>
+                  <Bar options={options} data={data} />
+                </div>
+              </SectionLayout>
             </div>
 
             <div className={s.rightSideContent}>
-              <SectionLayout title='Productos más vendidos'></SectionLayout>
+              <SectionLayout title='Productos más vendidos'>
+                <div className={s.chartContainer}>
+                  <Pie data={data} />
+                </div>
+              </SectionLayout>
             </div>
           </div>
         </div>
