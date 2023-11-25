943e3239cd1dcb5d951439bee56f96e5d742dab0
Neil Alvarez Garcia <Neil8b.jjr2014@gmail.com>
2023-11-25 03:22:22 -0500

    profile page

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -2,6 +2,7 @@ import { Logout, User } from '@/commons/icons';
 import { LOGIN, PROFILE } from '@/helpers/const';
 import { useOpenModal } from '@/hooks';
 import s from '@/styles/profile.module.css';
+import Image from 'next/image';
 import Link from 'next/link';
 
 export const Profile = () => {
@@ -11,6 +12,8 @@ export const Profile = () => {
     <>
       <div className={s.profile} onClick={openModal}>
         <p>user@gmail.com</p>
+
+        <Image src='/profile.png' alt='profile' width={50} height={50} className={s.image} />
       </div>
 
       <section className={`${s.modal} ${addOpen && s.active}`} onClick={closeModal}>
--- src/pages/perfil.tsx
+++ src/pages/perfil.tsx
@@ -1,5 +1,8 @@
 import { DashboardLayout } from '@/commons/layouts';
 import Head from 'next/head';
+import Image from 'next/image';
+
+import s from '@/styles/profile.module.css';
 
 const Perfil = () => {
   return (
@@ -9,7 +12,34 @@ const Perfil = () => {
       </Head>
 
       <DashboardLayout>
-        <p>acá irá info del usuario</p>
+        <h2 className={s.title}>Detalle del perfil</h2>
+
+        <section className={s.wrapper}>
+          <Image
+            src='/profile.png'
+            alt='profile'
+            width={100}
+            height={100}
+            className={s.profileImage}
+          />
+
+          <div className={s.userDetails}>
+            <div className={s.group}>
+              <h4>Nombre</h4>
+              <p>Admin</p>
+            </div>
+
+            <div className={s.group}>
+              <h4>Correo electrónico</h4>
+              <p>user@gmail.com</p>
+            </div>
+
+            <div className={s.group}>
+              <h4>Role</h4>
+              <p>Admin</p>
+            </div>
+          </div>
+        </section>
       </DashboardLayout>
     </>
   );
